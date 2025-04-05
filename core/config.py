ee2a84cd67e236793ced3e79323d6c305a96559f
getcerts1 <barrowtech1999@gmail.com>
2025-04-05 22:04:37 +0200

    repo lacking sensitive data

--- core/config.py
+++
@@ -1,6 +0,0 @@
-url = "postgresql://postgres:postgreuser@localhost:5432/ExpenseTrackerDatabase"
-
-
-ALGORITHM = "HS256"
-SECRET = "XQuAyvo5sPHe6MXQpDsvLWWPXYDqTHXO7tjwdQl/FYY="
-EXPIRATION_TIME = 30
