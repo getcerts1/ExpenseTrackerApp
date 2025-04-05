2dd7ce6d7a749e4776767a4c1d67e1d71bc028f5
getcerts1 <barrowtech1999@gmail.com>
2025-04-05 19:56:45 +0200

    created some get endpoints and made sure the db connection and querying works fine with the authentication

--- api/v1/expenses_routes.py
+++ api/v1/expenses_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from db.database import get_db
-from crud.expenses_crud import create_expense_crud
+from crud.expenses_crud import create_expense_crud, get_list_categories, get_expense_by_id_crud
 from schemas.expenseschema import CreateExpense, ReturnExpense
 from core.security import verify_token
 
@@ -21,3 +21,26 @@ async def create_expense(schema: CreateExpense, db: Session = Depends(get_db),
     user_id = user.id
     expense = create_expense_crud(schema_dict, user_id, db)
     return ReturnExpense.model_validate(expense)
+
+
+@router.get("/categories/{category}")
+async def list_by_category(category: str, db: Session = Depends(get_db),
+                           user: dict = Depends(verify_token)
+                           ):
+
+    user_id = user.id
+    if category == "Food&Dining":
+        category = "Food & Dining"
+    output = get_list_categories(category, user_id,db)
+
+    return output
+
+
+@router.get("/expense/{id}", response_model=ReturnExpense)
+async def get_expense_by_id(id: int, db: Session = Depends(get_db),
+                            user: dict = Depends(verify_token)):
+
+    user_id = user.id
+    expense = get_expense_by_id_crud(id,user_id, db)
+
+    return expense
--- crud/expenses_crud.py
+++ crud/expenses_crud.py
@@ -2,7 +2,12 @@ from fastapi import HTTPException, status, Depends
 from sqlalchemy.orm import Session
 from models.expenses import Expense
 
-
+VALID_CATEGORIES = [
+"Food & Dining",
+"Transportation",
+"Housing",
+"Entertainment",
+"Healthcare"]
 
 
 def create_expense_crud(schema, user_id: int, db: Session):
@@ -10,5 +15,33 @@ def create_expense_crud(schema, user_id: int, db: Session):
     db.add(expense)
     db.commit()
     db.refresh(expense)
+
+    return expense
+
+
+def get_list_categories(category: str, user_id: int, db: Session):
+    if category not in VALID_CATEGORIES:
+        raise HTTPException(status_code=400, detail="Invalid category")
+
+    category_expenses = db.query(Expense).filter(
+        (Expense.user_id == user_id) & (Expense.category == category)
+    ).all()
+
+    if not category_expenses:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No expenses found for category '{category}'"
+        )
+
+    return category_expenses
+
+
+def get_expense_by_id_crud(id: int,user_id, db: Session):
+    expense = db.query(Expense).filter(
+        (Expense.id == id) & (Expense.user_id == user_id)
+    ).first()
+
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="expense not found")
 
     return expense
